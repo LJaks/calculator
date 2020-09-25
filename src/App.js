402e5fed8fd2427dff71e69b40b6037679bb983b
Laura J <laura.jakseboge@gmail.com>
2020-09-25 17:57:08 +0200

    Basic concept of calculator is done.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      input: ""
+    }
+    this.sum = this.sum.bind(this)
+    this.reset = this.reset.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  reset(){
+    this.setState({
+        count: 0
+      })
+  }
+  sum() {
+    this.setState(prevState => {
+      return {
+        count: Math.floor(prevState.count + this.state.input)
+      }
+    })
+  }
+  handleChange(event) {
+    this.setState({
+          input: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.count}</h1>
+        <input value={this.state.input} onChange={this.handleChange}></input>
+        <button onClick={this.sum}>Sum!</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    );
+  }
 }
 
 export default App;
