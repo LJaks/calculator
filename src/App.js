265ed1b95d0e1b12130537d450d021d3c6218d76
Laura J <laura.jakseboge@gmail.com>
2020-10-15 20:18:19 +0200

    Updated styles in css file;

--- src/App.js
+++ src/App.js
@@ -1,65 +1,87 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Buttons from "./Buttons";
-import Display from "./Display"
+import Display from "./Display";
 
 function App() {
-    const [ currSum, setCurrSum ] = useState(0);
-    const [ clear, setClear ] = useState(false);
+  const [currSum, setCurrSum] = useState(0);
+  const [clear, setClear] = useState(false);
 
-    const Add = (e) => {
-        e.preventDefault();
-        if (clear) setClear(false);
-        let currNum = document.querySelector("#num").value;
-        if (currNum === "") return;
-        let sum = currSum + parseInt(currNum);
-        setCurrSum(sum);
-        document.querySelector("#num").value="";
-    };
-    const Clear = (e) => {
-        e.preventDefault();
-        console.log('sum:', currSum);
-        document.querySelector('form').reset();
-        setClear(true);
-        setCurrSum(0);
-    };
+  const Add = (e) => {
+    e.preventDefault();
+    if (clear) setClear(false);
+    let currNum = document.querySelector("#num").value;
+    if (currNum === "") return;
+    let sum = currSum + parseInt(currNum);
+    setCurrSum(sum);
+    document.querySelector("#num").value = "";
+  };
+  const Clear = (e) => {
+    e.preventDefault();
+    console.log("sum:", currSum);
+    document.querySelector("form").reset();
+    setClear(true);
+    setCurrSum(0);
+  };
 
-    useEffect(() => {
-        document.querySelector("#result").value = "";
-    },[]);
-    useEffect(() => {
-        if(clear)
-        document.querySelector("#result").value = "";
-    });
+  useEffect(() => {
+    document.querySelector("#result").value = "";
+  }, []);
+  useEffect(() => {
+    if (clear) document.querySelector("#result").value = "";
+  });
 
-    const MathSigns = (e) => {
-        e.preventDefault();
-        let currNum = document.querySelector("#num").value;
-        let sum;
-        if (document.querySelector("#result").value === "") {
-            sum = currSum + parseInt(currNum);
-        }
-        else (sum = currSum - parseInt(currNum));
-        setCurrSum(sum);
-        document.querySelector("#num").value="";
-    }
+  const MathSigns = (e) => {
+    e.preventDefault();
+    let currNum = document.querySelector("#num").value;
+    let sum;
+    if (document.querySelector("#result").value === "") {
+      sum = currSum + parseInt(currNum);
+    } else sum = currSum - parseInt(currNum);
+    setCurrSum(sum);
+    document.querySelector("#num").value = "";
+  };
 
-        return(
-            <div className="app">
-                <div className="app-title">
-                    <h1>Simple Calculator</h1>
-                </div>
-                <form className="calc-wrapper">
-                    {/* <input type="text" id="result" value={currSum} readOnly />
+  return (
+    <div className="app">
+      <div className="app-title">
+        <h1>Simple Calculator</h1>
+      </div>
+      <form className="calc-wrapper">
+        {/* <input type="text" id="result" value={currSum} readOnly />
                     <input type="text" id="num" placeholder="Enter a Number" />
                     <button onClick={Add}>Add</button>
                     <button onClick={Clear}>Clear</button> */}
-                
-                <Display value={currSum} />
-                <Buttons onClickFunction={Add} keyValue="+" />
-                <Buttons onClickFunction={Clear} keyValue="C" />
-                <Buttons onClickFunction={MathSigns} id="minus" keyValue="-" />
-                </form>
-            </div>
-        )
+
+        <div className="display">
+          <Display value={currSum} />
+        </div>
+        <div className="buttons">
+          <Buttons onClickFunction={Clear} keyValue="AC" />
+          <Buttons onClickFunction={0} keyValue="+/-" />
+          <Buttons onClickFunction={0} keyValue="%" />
+          <Buttons onClickFunction={0} keyValue="/" />
+
+          <Buttons onClickFunction={0} keyValue="7" />
+          <Buttons onClickFunction={0} keyValue="8" />
+          <Buttons onClickFunction={0} keyValue="9" />
+          <Buttons onClickFunction={0} keyValue="x" />
+
+          <Buttons onClickFunction={0} keyValue="4" />
+          <Buttons onClickFunction={0} keyValue="5" />
+          <Buttons onClickFunction={0} keyValue="6" />
+          <Buttons onClickFunction={MathSigns} keyValue="-" />
+
+          <Buttons onClickFunction={0} keyValue="1" />
+          <Buttons onClickFunction={0} keyValue="2" />
+          <Buttons onClickFunction={0} keyValue="3" />
+          <Buttons onClickFunction={Add} keyValue="+" />
+
+          <Buttons onClickFunction={0} keyValue="0" />
+          <Buttons onClickFunction={0} keyValue="," />
+          <Buttons onClickFunction={0} keyValue="=" />
+        </div>
+      </form>
+    </div>
+  );
 }
 export default App;
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function Buttons(props) {
-    return (
-    <button onClick={props.onClickFunction}>{props.keyValue}</button>
-    )
+  return (
+    <button
+      onClick={props.onClickFunction}
+      className={
+        props.keyValue === "0" ? "zero" : ((props.keyValue === "/" || props.keyValue === "x" || props.keyValue === "-" || props.keyValue === "+" || props.keyValue === "=" ) ? "right": (props.keyValue === "AC" || props.keyValue === "+/-" || props.keyValue === "%" ? "first-line" : ""))}
+    >
+      {props.keyValue}
+    </button>
+  );
 }
 export default Buttons;
